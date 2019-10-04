50c4387f849befa832470f17e8aea407b84fdedf
Lizeth23062018 <51805969+Lizeth23062018@users.noreply.github.com>
2019-10-03 23:11:22 -0500

    script mensaje

--- View/template/footer.php
+++ View/template/footer.php
@@ -52,6 +52,16 @@
 <!--sweetalert-->
 <script src="sweetalert/sweetalert.min.js"></script>
 <script src="sweetalert/mensajesweetalert.js"></script>
-
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'b4d297c1dbcbde2672a5b2b54f4229bb8c62a3b5';
+window.smartsupp||(function(d) {
+        var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+        s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+        c.type='text/javascript';c.charset='utf-8';c.async=true;
+        c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
 </body>
 </html>

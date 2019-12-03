d9f9d967f221557acbc2667b6cdc22165608076c
Yiber-Eulices <51768297+Yiber-Eulices@users.noreply.github.com>
2019-12-03 02:09:33 -0500

    camara

--- View/js/EveryBody/Perfil.js
+++ View/js/EveryBody/Perfil.js
@@ -175,8 +175,7 @@ $(document).ready(function(){
             width: 320,
             height: 240,
             image_format: 'jpeg',
-            jpeg_quality: 90,
-            force_flash: true
+            jpeg_quality: 90
         });
         Webcam.attach( '#my_camera' );
         $("#ModalCamara").modal();
